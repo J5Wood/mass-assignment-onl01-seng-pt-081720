ee064e199fc161ae41726b4f35f072a1247a5f17
Jeremy Wood <js.wood206@gmail.com>
2020-09-21 22:27:54 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,6 +5,9 @@ class Person
   :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
-    attributes.each { |key,value| self.send(("#{key}="), value)}
+    attributes.each do |key,value| 
+      self.send(("#{key}="), value)
+      binding.pry
+    end
   end
 end
